285ec64911ebf0941d599c443b5a52ef8fae6e73
Saaswath Kumar <saaskum12@gmail.com>
2024-10-27 16:20:15 -0400

    Finished save method for destination form

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from models import TravelPlan, Destination
 import uuid
 
+
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
@@ -13,16 +14,17 @@ class UserLoginForm(AuthenticationForm):
 
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Password'}))
-    
+
+
 class PlanForm(forms.ModelForm):
     class Meta:
         model = TravelPlan
         fields = [
-            'plan_name', 
-            'group_size', 
-            'trip_description', 
-            'jpg_upload_file', 
-            'txt_upload_file', 
+            'plan_name',
+            'group_size',
+            'trip_description',
+            'jpg_upload_file',
+            'txt_upload_file',
             'pdf_upload_file',
         ]
         labels = {
@@ -68,7 +70,7 @@ class PlanForm(forms.ModelForm):
 
     def save(self, commit=True, user=None):
         travel_plan = super().save(commit=False)
-        
+
         # Generate a unique group code
         unique_code = str(uuid.uuid4())[:8]  # Generate an 8-character unique code
         travel_plan.primary_group_code = unique_code
@@ -82,64 +84,68 @@ class PlanForm(forms.ModelForm):
             travel_plan.users.add(user)  # Add the user to the many-to-many relationship
 
         return travel_plan
- class DestinationForm(forms.Form):
-     class Meta:
-         model = Destination
-         fields = [
-             'destination_name',
-             'destination_description',
-             'jpg_upload_file',
-             'txt_upload_file',
-             'pdf_upload_file',
-         ]
-         labels = {
-             'jpg_upload_file': 'Upload an image for the destination',
-             'txt_upload_file': 'Upload a text file with destination details',
-             'pdf_upload_file': 'Upload a PDF document',
-         }
-         widgets = {
-             'destination_name': forms.TextInput(attrs={
-                 'class': 'form-control',
-                 'id': 'exampleFormControlInput1',
-                 'placeholder': 'Enter your destination name'
-             }),
-             'destination_description': forms.Textarea(attrs={
-                 'class': 'form-control',
-                 'id': 'exampleFormControlTextarea1',
-                 'rows': 3,
-                 'placeholder': 'Details about the destination'
-             }),
-             'jpg_upload_file': forms.ClearableFileInput(attrs={
-                 'class': 'form-control',
-                 'id': 'jpg_upload_file',
-                 'accept': '.jpg',
-             }),
-             'txt_upload_file': forms.ClearableFileInput(attrs={
-                 'class': 'form-control',
-                 'id': 'txt_upload_file',
-                 'accept': '.txt',
-             }),
-             'pdf_upload_file': forms.ClearableFileInput(attrs={
-                 'class': 'form-control',
-                 'id': 'pdf_upload_file',
-                 'accept': '.pdf',
-             }),
-         }
 
-     def save(self, commit=True, user=None):
-         destination = super().save(commit=False)
 
-         if user is not None:
-             destination.user = user  # Set the user field
+class DestinationForm(forms.Form):
+    class Meta:
+        model = Destination
+        fields = [
+            'destination_name',
+            'destination_description',
+            'jpg_upload_file',
+            'txt_upload_file',
+            'pdf_upload_file',
+        ]
+        labels = {
+            'jpg_upload_file': 'Upload an image for the destination',
+            'txt_upload_file': 'Upload a text file with destination details',
+            'pdf_upload_file': 'Upload a PDF document',
+        }
+        widgets = {
+            'destination_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'id': 'exampleFormControlInput1',
+                'placeholder': 'Enter your destination name'
+            }),
+            'destination_description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'id': 'exampleFormControlTextarea1',
+                'rows': 3,
+                'placeholder': 'Details about the destination'
+            }),
+            'jpg_upload_file': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'id': 'jpg_upload_file',
+                'accept': '.jpg',
+            }),
+            'txt_upload_file': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'id': 'txt_upload_file',
+                'accept': '.txt',
+            }),
+            'pdf_upload_file': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'id': 'pdf_upload_file',
+                'accept': '.pdf',
+            }),
+        }
 
-        #Todo: Set the travel_plan
+    def save(self, commit=True, travel_plan=None):
+        destination = super().save(commit=False)
 
-         destination.save()
+        # if user is not None:
+        #     destination.user = user  # Set the user field
 
-         if user is not None:
-             destination.users.add(user)  # Add the user to the many-to-many relationship
+        # Todo: Set the travel_plan
 
-         return destination
+        if travel_plan is not None:
+            destination.travel_plan = travel_plan
+
+        # if user is not None:
+        #     destination.users.add(user)  # Add the user to the many-to-many relationship
+
+        return destination
+
+
 class JoinGroupForm(forms.Form):
     group_code = forms.CharField(max_length=64, label='Group Code')
-
