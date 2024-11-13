289888f1c27b33ac2b9ffd6e30bb2be5303b3752
Ivan Kisselev <ivankisselev7@gmail.com>
2024-11-12 22:03:33 -0500

    fixed minor issue with cancel redirect

--- users/views.py
+++ users/views.py
@@ -87,6 +87,10 @@ class TravelPlanUpdateView(generic.UpdateView):
     def get_success_url(self):
         primary_group_code = self.object.primary_group_code
         return reverse_lazy('detail', kwargs={'primary_group_code': primary_group_code})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['primary_group_code'] = self.object.primary_group_code
+        return context
 
 def destination_creation(request, plan_id):
     if request.user.is_authenticated:
